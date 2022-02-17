7a56be11ca45418175892b43d90a5c5531e41883
shilpas1003 <shilpas1003@gmail.com>
2022-02-17 13:41:54 +0530

    Initial Commit - Beginner

--- CountOfPairs.py
+++ CountOfPairs.py
@@ -2,17 +2,17 @@ class Solution:
     # @param A : list of integers
     # @return an integer
     def firstMissingPositive(self, A):
-        # sorted = True
-        # print(A)
-        # while sorted:
-        #     sorted = False
-        #     for i in range(len(A)-1):
-        #         if A[i] > A[i+1]:
-        #             A[i], A[i+1] = A[i+1], A[i]
-        #             sorted = True
-        # k = A[0]
-        # for j in range(len(A)-1):
-        #     k = A[j]
+        sorted = True
+        print(A)
+        while sorted:
+            sorted = False
+            for i in range(len(A)-1):
+                if A[i] > A[i+1]:
+                    A[i], A[i+1] = A[i+1], A[i]
+                    sorted = True
+        k = A[0]
+        for j in range(len(A)-1):
+            k = A[j]
 
 
 
